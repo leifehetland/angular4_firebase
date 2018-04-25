1b52e88182f60583332709d21907ff7f2cb6e23a
Leif Hetland <lehetland1@gmail.com>
2018-04-24 22:43:09 -0500

    finally got adding updating and deleting to play nice with Firebase

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ https://stackoverflow.com/questions/47529286/invalidpipeargument-object-object-f
 https://github.com/angular/angularfire2/issues/1571
 
 https://stackoverflow.com/questions/48597037/error-class-firebaseapp-incorrectly-implements-interface-app?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
+## Extremely Helpful Link!!!
+https://github.com/angular/angularfire2/blob/master/docs/rtdb/lists.md#deleting-the-entire-list
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
 import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +10,31 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  classes$;
-  classList$: AngularFireList<{}>;
-  // oneClass$;
-  // author$;
+  classesRef: AngularFireList<{}>;
+  classes: Observable<any[]>;
 
-  constructor(db: AngularFireDatabase) {
-    this.classList$ = db.list('/classes');
-    this.classes$ = this.classList$.valueChanges();
+  constructor(private db: AngularFireDatabase) {
+    this.classesRef = db.list('/classes');
+    this.classes = this.classesRef
+      .snapshotChanges()
+      .map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      });
   }
 
-  add(oneClass: HTMLInputElement) {
-    this.classList$.push(oneClass.value);
-    oneClass.value = '';
-
+  add(newClass: HTMLInputElement) {
+    this.classesRef.push( {text: newClass});
   }
+
+  update(key: string, newText: string) {
+    this.classesRef.update(key, { text: newText });
+  }
+
+  delete(key: string) {
+    this.classesRef.remove(key);
+  }
+
+  deleteEverything() {
+   this.classesRef.remove();
+ }
 }
